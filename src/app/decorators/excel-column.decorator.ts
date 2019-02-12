d743e49f020e20f5ae3e98eced1f3d0fdceb675b
jinderSingh <kuljindergojlia@gmail.com>
2019-02-12 20:58:16 +0100

    excel column property decorator

---
+++ src/app/decorators/excel-column.decorator.ts
@@ -0,0 +1,15 @@
+export const excelMetadata = 'EXCEL_TO_OBJECT';
+
+export function excelColumn(targetPropertyName: string) {
+  return function(target, key) {
+    const metadata = target[excelMetadata] || {};
+    metadata[key] = targetPropertyName;
+
+    Object.defineProperty(target, excelMetadata, {
+      writable: true,
+      enumerable: true,
+      configurable: true,
+      value: metadata
+    });
+  };
+}
