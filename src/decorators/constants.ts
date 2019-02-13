82c4f919625e4a42b317ae71a323cd9ad8b76b21
jinderSingh <kuljindergojlia@gmail.com>
2019-02-13 20:46:42 +0100

    posibility to pass transfom method

--- src/decorators/constants.ts
+++ src/decorators/constants.ts
@@ -1,1 +1,2 @@
-export const excelMetadata = 'EXCEL_TO_OBJECT';
+export const EXCEL_METADATA = 'EXCEL_TO_OBJECT';
+export const ORIGINAL_VALUE = 'ORIGINAL_VALUE';
--- src/decorators/excel-column.decorator.ts
+++ src/decorators/excel-column.decorator.ts
@@ -1,16 +1,34 @@
-import { excelMetadata } from './constants';
+import { EXCEL_METADATA, ORIGINAL_VALUE } from './constants';
 
-export function excelColumn(targetPropertyName: string) {
-  return function(target, key) {
-    const metadata = target[excelMetadata] || {};
-    metadata[key] = targetPropertyName;
-
-    Object.defineProperty(target, excelMetadata, {
+export function excelColumn(targetPropertyName: string, transformer: Function) {
+  return function (target, key) {
+    const originalValue = target[key];
+    const metadata = target[EXCEL_METADATA] || {};
+    const properties = {
       writable: true,
       enumerable: true,
-      configurable: true,
+      configurable: true
+    }
+    metadata[key] = targetPropertyName;
+
+    if (transformer && typeof transformer === 'function') {
+
+      Object.defineProperty(target, ORIGINAL_VALUE, {
+        ...properties,
+        value: originalValue
+      })
+
+      if (delete target[key]) {
+        Object.defineProperty(target, key, {
+          ...properties,
+          value: transformer.call(undefined, originalValue)
+        })
+      }
+    }
+
+    Object.defineProperty(target, EXCEL_METADATA, {
+      ...properties,
       value: metadata
     });
   };
 }
-
