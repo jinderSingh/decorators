f276288ae5d0ba91fc75f6d971a8b7729bc2ef4c
jinderSingh <kuljindergojlia@gmail.com>
2019-02-13 23:06:26 +0100

    separted function for parsing results to target class type

--- src/decorators/excel-row.decorator.ts
+++ src/decorators/excel-row.decorator.ts
@@ -11,31 +11,10 @@ export function excelRows < T > (targetClass: new() => T) {
       headers,
       results
     }) {
-      value = results.reduce((prev, next) => {
-        const newInstanceOfTargetClass = new targetClass();
-        const metadata = newInstanceOfTargetClass[EXCEL_METADATA];
-        
-        headers.forEach((header, index) => {
-          const mappedPropertyName = getKeyIfValueIsEqualTo(header, metadata);
-          if (mappedPropertyName) {
-            const originalValue = next[index];
-            const transformer = newInstanceOfTargetClass[CELL_VALUE_TRANSFORMER] && newInstanceOfTargetClass[CELL_VALUE_TRANSFORMER][mappedPropertyName];
-
-            let valueToSet = originalValue;
-
-            if (transformer) {
-              valueToSet = transformer.call(undefined, valueToSet);
-            }
-
-            newInstanceOfTargetClass[mappedPropertyName] = valueToSet;
-          }
-        });
-
-        return prev.concat(newInstanceOfTargetClass);
-      }, []);
+      value = mapValuesToTargetTypeObjects(headers, results, targetClass);
     };
 
-    
+
     if (delete target[key]) {
       Object.defineProperty(target, key, {
         get: getter,
@@ -46,6 +25,31 @@ export function excelRows < T > (targetClass: new() => T) {
     }
   };
 }
+
+export function mapValuesToTargetTypeObjects(headers, results, targetClass: new () => any) {
+  return results.reduce((prev, next) => {
+    const newInstanceOfTargetClass = new targetClass();
+    const metadata = newInstanceOfTargetClass[EXCEL_METADATA];
+
+    headers.forEach((header, index) => {
+      const mappedPropertyName = getKeyIfValueIsEqualTo(header, metadata);
+      if (mappedPropertyName) {
+        const originalValue = next[index];
+        const transformer = newInstanceOfTargetClass[CELL_VALUE_TRANSFORMER] && newInstanceOfTargetClass[CELL_VALUE_TRANSFORMER][mappedPropertyName];
+
+        let valueToSet = originalValue;
+
+        if (transformer) {
+          valueToSet = transformer.call(undefined, valueToSet);
+        }
+
+        newInstanceOfTargetClass[mappedPropertyName] = valueToSet;
+      }
+    });
+
+    return prev.concat(newInstanceOfTargetClass);
+  }, []);
+};
 
 function getKeyIfValueIsEqualTo(valueToCompare, metadata): string | undefined {
 
