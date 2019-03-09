9ec7df1eef4095ad691e2c00511d5918d058329d
jinderSingh <kuljindergojlia@gmail.com>
2019-03-10 00:34:38 +0100

    new method added  to convert object to excel format data [][]

--- src/util-methods.ts
+++ src/util-methods.ts
@@ -1,4 +1,4 @@
-import { EXCEL_METADATA } from "./decorators/constants";
+import { COLUMN_NAMES, COLUMN_NUMBERS, EXCEL_METADATA, PROP } from "./decorators/constants";
 
 /**
  * checks if object has property
@@ -83,7 +83,7 @@ export function isArrayNotEmpty(arr: []): boolean {
  * @param {T} value
  * @returns {boolean}
  */
-export function hasValue<T>(value: T): boolean {
+export function hasValue < T > (value: T): boolean {
     return value !== null && value !== undefined;
 }
 
@@ -93,7 +93,43 @@ export function hasValue<T>(value: T): boolean {
  * @param objectArrays 
  * @param classType 
  */
-export function objectToExcel(objectArrays: any[], classType: new () => any): any[][] {
+export function objectToExcel(classType: new() => any): (arr: []) => any[][] {
     const metadata = classType.prototype[EXCEL_METADATA];
-    return null;
-}
+
+    // {COLUMN_NAME: {name: {prop: 'name', transformer: null}}, COLUMN_NUMBER:{ name: {prop: 1, transformer: null}}}
+
+    if (!hasValue(metadata)) {
+        throw new Error(`Metadata not found on class ${classType.constructor.name}. Please annotate class type with @excelColumn decorator.`);
+    }
+
+    const shouldUseColumnNumbers = hasValue(metadata[COLUMN_NUMBERS]);
+
+    const metadataToUse = metadata[shouldUseColumnNumbers ? COLUMN_NUMBERS : COLUMN_NAMES];
+
+    const properties = Object.keys(metadataToUse);
+
+    const headers = shouldUseColumnNumbers ? null : properties
+        .reduce((old, newVal) => old.concat(metadataToUse[newVal][PROP]), []);
+
+    return (objectArrays: any[]) => {
+        if (!hasValue(objectArrays)) {
+            return [];
+        }
+        let result = [];
+
+        if (headers) {
+            result.push(headers);
+        }
+
+        return objectArrays.reduce((prev, next) => {
+            const row = properties.reduce((previous, key, index) => {
+                const valueIndex = shouldUseColumnNumbers ? +metadataToUse[key].prop : index;
+                previous[valueIndex] = next[key];
+                return previous;
+            }, []);
+            
+            prev.push(row);
+            return prev;
+        }, result);
+    };
+};
